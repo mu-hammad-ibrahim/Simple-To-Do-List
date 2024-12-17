8f60640e4e0c362458e5dc4b8da0a3858de07791
mu-hammad-ibrahim <mohamed_ibrahim1999@outlook.com>
2024-12-17 21:19:35 +0200

    add genericRepo

---
+++ To-Do List.BLL/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using To_Do_List.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List.BLL.Repositories.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IQueryable<T>> GetAll();
+
+        Task<T> GetByIdAsync(int id);
+
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ To-Do List.BLL/Repositories/Repositories/GenericReposiory.cs
@@ -0,0 +1,51 @@
+﻿using To_Do_List.BLL.Repositories.Interfaces;
+using To_Do_List.DAL.Models;
+using To_Do_List.DAL.Models.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List.BLL.Repositories.Repositories
+{
+    public class GenericReposiory<T> : IGenericRepository<T> where T : class
+    {
+        private protected readonly ToDoListContext _dbContext;
+
+        public GenericReposiory(ToDoListContext dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IQueryable<T>> GetAll()
+        {
+            return (IQueryable<T>)_dbContext.Set<T>().AsNoTracking();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
